f265c35d056e7e2813e47d4343ba5cfe97f2bb4d
RadziSzop <radek.krochmall@gmail.com>
2023-10-06 12:13:42 +0200

    app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { Button } from "./components/ui/button";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import { Login } from "./pages/Login/Login";
-import { ModeToggle } from "./components/theme-provider/mode-toggle";
 import { RegisterForm } from "./components/register-form/register-form";
 
 function App() {
   return (
     <>
       <Navbar />
-      <ModeToggle />
-      {
+      <div className="h-[calc(100vh-57px)]">
         <Routes>
           <Route path="/books" element={<RegisterForm />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-      }
-
+      </div>
     </>
   );
 }
